4d332ffc435b118041d1b2d0df18751b457e8582
student <student@DESKTOP-OT59EC7>
2019-10-11 10:18:58 +0200

    LAB

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -86,6 +86,7 @@ namespace Lab2
                 }
                 if (licznik == 5) {
                     Console.WriteLine("Dość na dziś");
+                    break;
                 }
 
             }
