f7d8986c0a812364128b2632d90dfc81bc3cdb3d
student <student@DESKTOP-OT59EC7>
2019-10-11 10:12:08 +0200

    LAB

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -25,40 +25,69 @@ namespace Lab2
             else { Console.WriteLine("Nie podano liczby"); }
             Console.ReadKey();
             */
-            /*
-            Console.WriteLine("Podaj Liczbe");
-            string odpowiedz = Console.ReadLine();
-            int liczba=Convert.ToInt32(odpowiedz);
-           Console.WriteLine((liczba % 2 == 0) ? "Parzysta" : "Nieparzysta");
+         /*
+         Console.WriteLine("Podaj Liczbe");
+         string odpowiedz = Console.ReadLine();
+         int liczba=Convert.ToInt32(odpowiedz);
+        Console.WriteLine((liczba % 2 == 0) ? "Parzysta" : "Nieparzysta");
 
-            Console.ReadKey();
-            */
-            /*
-            double odleglosc = 1;
-            double suma = 0;
-            int licznik = 0;
-            while (odleglosc != 0)
-            {
-                string odleglosctekst = Console.ReadLine();
-                odleglosc = Convert.ToDouble(odleglosctekst);
-                suma += odleglosc;
-                licznik++;
-            }
-            double wynik = suma / (licznik -1) ;
-            Console.WriteLine(wynik);
-            */
-            int licznik=1;
-            string linia= Console.ReadLine();
-            while (linia !="exit") {
+         Console.ReadKey();
+         */
+         /*
+         double odleglosc = 1;
+         double suma = 0;
+         int licznik = 0;
+         while (odleglosc != 0)
+         {
+             string odleglosctekst = Console.ReadLine();
+             odleglosc = Convert.ToDouble(odleglosctekst);
+             suma += odleglosc;
+             licznik++;
+         }
+         double wynik = suma / (licznik -1) ;
+         Console.WriteLine(wynik);
+         */
+         /*
+         int licznik=1;
+         string linia= Console.ReadLine();
+         while (linia !="exit") {
+             Console.WriteLine(licznik++);
+             linia= Console.ReadLine();
+             if (licznik == 5) {
+                 Console.WriteLine("Wyprodukowano maksymalna ilość");
+                 break;
+             } 
+             */
+            int licznik = 1;
+            int[] liczniki = {1, 1, 1};
+            string linia = Console.ReadLine();
+            while (linia != "exit") {
                 Console.WriteLine(licznik++);
-                linia= Console.ReadLine();
-                if (licznik == 5) {
-                    Console.WriteLine("Wyprodukowano maksymalna ilość");
-                    break;
-                } 
+                linia = Console.ReadLine();
+                switch (linia)
+                {
+                    case "A":
+                        Console.WriteLine(liczniki[0]++);
+                        break;
+                    case "B":
+                        Console.WriteLine(liczniki[1]++);
+                        break;
+                    case "C":
+                        Console.WriteLine(liczniki[2]++);
+                        break;
+                    default:
+                        Console.WriteLine("zly kod porduktu");
+                        break;
+
+
+                }
+
+            }
+
+             
             }
 
         }
     }
-}
+
 
