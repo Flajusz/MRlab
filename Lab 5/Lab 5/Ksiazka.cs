67397ee0761f536c0cace2c1f587adfc686c3685
Flajusz <flajuniunio@gmail.com>
2019-11-08 19:41:50 +0100

    LAb 5

---
+++ Lab 5/Lab 5/Ksiazka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    public class Ksiazka
+    {
+        public string Tytul;
+        public string Autor;
+        public int Regal;
+        public int Polka;
+        public int Miejsce;
+
+
+
+        public Ksiazka(string tytul, string autor, int regal, int polka, int miejsce)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Regal = regal;
+            Polka = polka;
+            Miejsce = miejsce;
+        }
+
+        public Ksiazka() { }
+    }
+}
--- Lab 5/Lab 5/Program.cs
+++ Lab 5/Lab 5/Program.cs
@@ -8,8 +8,77 @@ namespace Lab_5
 {
     class Program
     {
+
+        // napisz porgram wspomagający magazyn bibilioteki
+        //kazda ksiazka ma tytul,magazyn ma 3 regaly 
+        //regal
+        //ma 6 polek,10 miejsc ,wskaz gdzie szukac ksiażki na podsstawie tytulu  
         static void Main(string[] args)
         {
+
+            Ksiazka[,,] a = new Ksiazka[3, 6, 10];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        a[i, j, k] = new Ksiazka("Pan Tadeusz", "Mickiewicz", i, j, k);
+
+                    }
+                }
+
+            }
+            a[1, 4, 6] = new Ksiazka("Potop", "Sienkiewicz", 1, 4, 6);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        Console.WriteLine("Regał[{0}],Półka[{1}],Miejsce[{2}]:", i, j, k);
+                        Console.WriteLine(a[i, j, k].Tytul + " , " + a[i, j, k].Autor);
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Czego szukasz ?");
+            string kon = Convert.ToString(Console.ReadLine());
+            int licznik = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (kon == a[i, j, k].Tytul || kon == a[i, j, k].Autor)
+                        {
+                            Console.WriteLine("Obiekt znajduje się w Regale {0}, Półce {1}, Miejscu {2}", i, j, k);
+                            licznik++;
+
+                        }
+
+
+
+                    }
+                }
+
+            }
+            if (licznik == 0)
+            {
+                Console.WriteLine("Nie znaleziono obiektu");
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }
+
