2bbeb5fbad3f7b3aa5b529f935be8f18485023b5
Flajusz <flajuniunio@gmail.com>
2019-11-19 21:44:45 +0100

    LAb

---
+++ Lab 6/Lab 6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+
+        /* static void opisz(Auto samochod)
+         {
+             Console.WriteLine(samochod.model+"; "+ samochod.przebieg+" km;"+ samochod.ilosc_drzwi+";" + samochod.waga+";"+ samochod.VIN+";");
+         }
+         */
+        static void opiszSerwer(Serwer strona)
+        {
+            Console.WriteLine(strona.SzybProcesora + "GHz, Łącze: " + strona.Lacze + ", " + strona.Ram + "GB, " + strona.Technologia);
+        }
+
+        static void Main(string[] args)
+        {
+            /*  
+        Auto mojeAuto = new Auto
+        (
+            100000,"A4",5,1000
+        );
+
+        Auto s1 = new Auto
+        (
+            200000, "Multipla", 5, 1500
+        );
+
+        Auto s2 = new Auto
+        (
+            150000, "Seicento", 4, 800
+        );
+
+        Auto nowy = new Auto
+        (
+           4, 2000
+        );
+
+        mojeAuto.zwiekszprzebieg(10000);
+        opisz(mojeAuto);
+        opisz(s1);
+        opisz(s2);
+        opisz(nowy);
+
+
+        */
+            Serwer Serwer_nr1 = new Serwer(
+               4, 5, 16, ".NET");
+            opiszSerwer(Serwer_nr1);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 6/Lab 6/Strona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Strona
+    {
+
+        //Dowolne rozszerzanie klasy, hermetyzacja wszystkiego co mozliwe
+        //Strona internetowa
+        //  Serwer
+        //      Szybkosc Procesora
+        //      Łącze
+        //      RAM
+        //      Technologia
+        //  Domena
+        //      Nazwa
+        //      Waznosc
+    }
+
+    class Serwer
+    {
+        public int SzybProcesora { get; private set; }
+        public int Lacze { get; private set; }
+        public int Ram { get; private set; }
+        public string Technologia { get; private set; }
+
+        public Serwer(int szybproc, int lacze, int ram, string technologia)
+        {
+            SzybProcesora = szybproc;
+            Lacze = lacze;
+            Ram = ram;
+            Technologia = technologia;
+        }
+    }
+
+    class Domena
+    {
+
+        public int Nazwa { get; private set; }
+        public DateTime Waznosc { get; private set; }
+
+        public Domena(int nazwa, DateTime waznosc)
+        {
+            Nazwa = nazwa;
+            Waznosc = waznosc;
+        }
+    }
+}
