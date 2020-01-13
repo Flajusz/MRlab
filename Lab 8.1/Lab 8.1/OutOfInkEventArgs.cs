c143994581d2d9b06bb521b0f732a5675f554ef2
Flajusz <flajuniunio@gmail.com>
2020-01-13 23:03:32 +0100

    Lab

---
+++ Lab 8.1/Lab 8.1/OutOfInkEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8._1
+{
+    class OutOfInkEventArgs
+    {
+        public class OutOfInkEventArgs : PrinterEventArgs
+        {
+
+            public OutOfInkEventArgs(string inkColor, int page) : base(page)
+            {
+                this.InkColor = inkColor;
+            }
+
+            public string InkColor { get; set; }
+
+        }
+
+    }
+}
---
+++ Lab 8.2/Lab 8.2/OutOfInkEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8._2
+{
+    public class OutOfInkEventArgs : PrinterEventArgs
+    {
+
+        public OutOfInkEventArgs(string inkColor, int page) : base(page)
+        {
+            this.InkColor = inkColor;
+        }
+
+        public string InkColor { get; set; }
+
+    }
+}
