8da90c6e8ce640c369d0e0e7db8dcff6481f3ce0
Flajusz <flajuniunio@gmail.com>
2020-02-02 15:42:57 +0100

    Lab

---
+++ Lab_13/Lab_13/IOReadercs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Lab13
+{
+    class IOReader:IReader
+    {
+        public List<Person> ReadPeople(string FileName)
+        {
+
+            var reader = new StreamReader(FileName);
+            string data = reader.ReadToEnd();
+            string[] lines = data.Split(new[] { '\n', '\r' });
+            string[] allData = File.ReadAllLines(FileName);
+            var people = allData.Select(x =>
+            CSVPersonToString.Convert(x)
+            );
+            return people.ToList();
+        }
+    }
+}
---
+++ Lab_13/Lab_13/IReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13
+{
+    interface IReader
+    {
+        List<Person> ReadPeople(string FileName);
+    }
+}
